0dd10bb0dac9cfc387242ce90d6263a1ce5fa975
Nadiar AS <codxse@gmail.com>
2018-06-22 18:15:44 +0700

    0.1.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject zenius-material "0.1.1-SNAPSHOT"
+(defproject zenius-material "0.1.1"
   :description "A ClojureScript reagent wrapper for Material-UI v1.x"
   :url "https://github.com/codxse/zenius-material"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/clojurescript "1.10.312"]
                  [reagent "0.8.1" :exclusions [cljsjs/react cljsjs/react-dom]]
-                 [cljsjs/material-ui "1.0.0-beta.40-0"]
-                 [cljs-react-material-ui "0.2.48"]])
+                 [cljsjs/material-ui "1.2.1-0"]
+                 [cljs-react-material-ui "0.2.50"]])
